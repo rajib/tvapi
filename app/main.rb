44c7f4de898ecbbd8a1285b8923e9c3da7f3dfd1
Rajib Chowdhury <rajib@rajib-laptop.(none)>
2010-04-08 00:48:42 +0530

    rackup.ru updated.

---
+++ app/main.rb
@@ -0,0 +1,38 @@
+require 'rubygems'
+require 'sinatra'
+require 'dm-core'
+require 'dm-validations'
+
+
+## CONFIGURATION
+configure :development do
+  DataMapper.setup(:default, {
+    :adapter  => 'mysql',
+    :host     => 'localhost',
+    :username => 'root' ,
+    :password => '',
+    :database => 'tv_development'})  
+
+  DataMapper::Logger.new(STDOUT, :debug)
+end
+
+### MODELS
+class Channel
+  include DataMapper::Resource
+  property :id,         Integer, :serial=>true
+  property :name,      String
+  property :logo,      String
+  property :url,      String
+end
+DataMapper.auto_upgrade!
+
+
+### CONTROLLER ACTIONS
+
+# index
+get '/' do
+  @channels = Channel.all
+  haml :index
+end
+
+
